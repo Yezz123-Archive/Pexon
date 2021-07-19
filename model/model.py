287fe193acbc4be49b0d3317d0373db44424bcac
Yasser Tahiri <yasserth19@gmail.com>
2021-07-19 03:59:40 +0100

    🔧 Delete unsupported files

--- model/model.py
+++ model/model.py
@@ -1,5 +1,27 @@
-# pydantic : pydantic enforces type hints at runtime, and provides user friendly errors when data is invalid.
-from pydantic import BaseModel, Schema
+from sqlalchemy import Column, Boolean, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql.schema import ForeignKey
+from data.database import Base
 
-class TodoItem(BaseModel):
-    title: str = Schema(..., description='Name of todo item', max_length=180)
+
+class User(Base):
+    __tablename__ = "users"
+
+
+id = Column(Integer, primary_key=True, index=True)
+lname = Column(String)
+fname = Column(String)
+email = Column(String, unique=True, index=True)
+todos = relationship("TODO", back_populates="owner",
+                     cascade="all, delete-orphan")
+
+
+class TODO(Base):
+    __tablename__ = "todos"
+
+
+id = Column(Integer, primary_key=True, index=True)
+text = Column(String, index=True)
+completed = Column(Boolean, default=False)
+owner_id = Column(Integer, ForeignKey("users.id"))
+owner = relationship("User", back_populates="todos")
